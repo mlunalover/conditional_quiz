9cde0d306d5043886cfd4e82793783564cb92c30
Matt Lunalover <mlunalover@gmail.com>
2015-07-23 13:48:52 -0400

    logic

---
+++ quiz.js
@@ -0,0 +1,6 @@
+var answer = prompt('What programming language is the name of a gem?');
+if ( answer.toLocaleUpperCase() === 'RUBY' ) {
+  document.write("<p>That's right!");
+} else  {
+  document.write("<p>Sorry, that's wrong.</p>");
+}
